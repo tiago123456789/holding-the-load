bbf5e4b1682b58cdb4cd2e2e6142a5a453ccbbe5
Tiago R. da costa <tiagorosadacost@gmail.com>
2026-01-04 15:39:22 -0300

    changes to make the people understand where needs to change the value

--- loadtest/publishTest.sh
+++ loadtest/publishTest.sh
@@ -2,5 +2,5 @@
 
 autocannon -a 5000 -c 600 -m POST -i ./loadtest/data.json \
   -H "Content-Type:application/json" \
-  -H "x-api-key: test" \
+  -H "x-api-key: api_key_value_here" \
   https://holding-theload.tiagorosadacost.workers.dev/new-events
